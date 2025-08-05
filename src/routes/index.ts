f4459ae38ce6030ff61f867b206cd4293e3b497b
Tharindu DM <tharindudm@hotmail.com>
2025-08-05 19:15:13 +0530

    feat: implement user management functionality with create, login, profile completion, and existence check endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import dummyRoutes from "./dummy.routes.js";
 import appointmentSlotsRoutes from "./appointmentSlots.route.js";
 import medicalEstablishmentsRoutes from "./medicalEstablishments.route.js";
+import userRoutes from "./user.routes.js";
 
 const router = Router();
 
 router.use("/dummies", dummyRoutes);
 router.use("/slots", appointmentSlotsRoutes);
 router.use("/medical-establishments", medicalEstablishmentsRoutes);
+router.use("/users", userRoutes);
 
 export default router;
