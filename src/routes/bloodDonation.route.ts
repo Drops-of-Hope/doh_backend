fac99d6f07b0a91a54aaa29a4c07290c9de459d5
Tharindu DM <tharindudm@hotmail.com>
2025-10-16 21:18:28 +0530

    fix: update import paths to include file extensions for consistency

--- src/routes/bloodDonation.route.ts
+++ src/routes/bloodDonation.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { BloodDonationController } from '../controllers/bloodDonation.controller';
+import { BloodDonationController } from '../controllers/bloodDonation.controller.js';
 
 const router = Router();
 
